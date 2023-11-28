05adefc5dc3231f2461bffb078ed568c543bb180
Slava Poliakov <78slavapolyakov@gmail.com>
2023-11-28 17:42:35 +0200

    Changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { concatenation } from './concatenation';
 
-const button = document.querySelector('button')!;
-const input = document.querySelector('input')!;
+const button = document.querySelector('button');
+const input = document.querySelector('input');
 
 if (button && input) {
   button.addEventListener('click', () => {
